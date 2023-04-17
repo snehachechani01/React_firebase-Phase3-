dc6cd3ea3e75c619ef532f43fe4ddf4d969a2b16
snehachechani01 <sneha.chechani@sigmainfo.net>
2023-04-17 11:31:14 +0530

    updated

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './components/Home/Home';
@@ -9,39 +9,31 @@ import ListUser from './components/ListUser';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import AuthDetails from './components/AuthDetails.jsx';
-import {auth} from './firebase';
+import { auth } from './firebase';
 function App() {
 
   const [userEmail, setUserEmail] = useState("");
   const [userName, setUserName] = useState("");
 
   useEffect(() => {
-      auth.onAuthStateChanged((user) => {
-          if (user) {
-              setUserName(user.displayName);
-              setUserEmail(user.email);
-          } else {
-              setUserEmail("");
-              setUserName("");
-          }
-      });
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+        setUserEmail(user.email);
+      } else {
+        setUserEmail("");
+        setUserName("");
+      }
+    });
   }, []);
   return (
     <div className="App">
-      
-      
-    
-
-      
-    
-      <BrowserRouter>
-      
-            
-        <Routes>
-        <Route path={'/Home'} element={<Home name={userName} email={userEmail}  />} />
-        <Route path={'/Signin'} element={<Signin />} />
-         <Route index element={<Signup />} />
-          <Route path="ListUser"element={<ListUser />} />
+    <BrowserRouter>
+       <Routes>
+          <Route path={'/Home'} element={<Home name={userName} email={userEmail} />} />
+          <Route path={'/Signin'} element={<Signin />} />
+          <Route index element={<Signup />} />
+          <Route path="ListUser" element={<ListUser />} />
           <Route path="Createuser" element={<CreateUser />} />
           {/* <Route path="user/:id/edit" element={<EditUser />} /> */}
           <Route path={"ListUser/:id/edit"} element={<EditUser />} />
